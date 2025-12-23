e3b633223e65dc08078fa8c585baf5e3dd46be5e
Mohammad Sahebi <msahebi.me@gmail.com>
2025-12-24 02:00:28 +0330

    implement password reset and change

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,9 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+# Email config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
 

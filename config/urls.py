93f90e034d43d9787c6d61c54cd4784f981250e8
Mohammad Sahebi <msahebi.me@gmail.com>
2025-12-24 16:23:42 +0330

    add BookListView

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
-    # for SignUp
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),  # for SignUp
+    path('books/', include('books.urls')),
 ]
